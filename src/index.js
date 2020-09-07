4c1677d47b8f9b0275fb647943bd3ca0c351add4
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-09-07 16:43:27 +0300

    add base store and reducer functions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import {Header} from "@/components/header/Header";
 import {Toolbar} from "@/components/toolbar/Toolbar";
 import {Formula} from "@/components/formula/Formula";
 import {Table} from "@/components/table/Table";
+import {createStore} from "core/createStore";
+import {reducer} from "@/redux/reducer";
 import './sass/index.sass'
+
+const store = createStore(reducer)
 
 const excel = new Excel('#app', {
     components: [Header, Toolbar, Formula, Table]
